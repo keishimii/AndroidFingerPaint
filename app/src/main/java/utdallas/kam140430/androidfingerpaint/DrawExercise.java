39126be2c4101e8870347acde11f899c31667f94
Keishi Mii <kam140430@utdallas.edu>
2017-04-13 12:26:54 -0500

    initial commit

---
+++ app/src/main/java/utdallas/kam140430/androidfingerpaint/DrawExercise.java
@@ -0,0 +1,13 @@
+package utdallas.kam140430.androidfingerpaint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawExercise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_exercise);
+    }
+}
