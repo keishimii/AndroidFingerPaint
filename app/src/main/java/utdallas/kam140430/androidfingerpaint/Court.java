855b7baadd0af00e1e75a7963c6b9e1d566e7e5a
Keishi Mii <kam140430@utdallas.edu>
2017-04-13 12:38:07 -0500

    add court

---
+++ app/src/main/java/utdallas/kam140430/androidfingerpaint/Court.java
@@ -0,0 +1,35 @@
+package utdallas.kam140430.androidfingerpaint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by keishi on 4/13/17.
+ */
+
+public class Court extends View {
+    private ShapeDrawable mDrawable;
+
+    public Court(Context context, AttributeSet attr){
+        super(context, attr);
+        int width = context.getResources().getDisplayMetrics().widthPixels;
+        int height = context.getResources().getDisplayMetrics().heightPixels;
+        int x = 0;
+        int y = context.getResources().getDisplayMetrics().heightPixels - width;
+
+        mDrawable = new ShapeDrawable(new RectShape());
+        mDrawable.getPaint().setColor(Color.BLUE);
+        mDrawable.setBounds(x, y, width, height);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        mDrawable.draw(canvas);
+    }
+}
