a519574577ba5bf5dec005490becf70fc90daef5
Keishi Mii <kam140430@utdallas.edu>
2017-04-18 17:50:17 -0500

    add draw feature

--- app/src/main/java/utdallas/kam140430/androidfingerpaint/Court.java
+++ app/src/main/java/utdallas/kam140430/androidfingerpaint/Court.java
@@ -2,35 +2,78 @@ package utdallas.kam140430.androidfingerpaint;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 
 /**
  * Created by keishi on 4/13/17.
  */
 
 public class Court extends View {
-    private ShapeDrawable mDrawable;
+
+    //paint colors
+    public int paintColor;
+    //drawing path
+    private Path drawPath;
+    //drawing and canvas paint
+    private Paint drawPaint, canvasPaint;
+    //canvas
+    private Canvas drawCanvas;
+    //canvas bitmap
+    private Bitmap canvasBitmap;
 
     public Court(Context context, AttributeSet attr){
+
         super(context, attr);
         int width = context.getResources().getDisplayMetrics().widthPixels;
         int height = context.getResources().getDisplayMetrics().heightPixels;
-        int x = 0;
-        int y = 0;
 
-        mDrawable = new ShapeDrawable(new RectShape());
-        mDrawable.getPaint().setColor(Color.LTGRAY);
-        mDrawable.setBounds(x, y, width, height);
+        canvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+
+        drawPaint = new Paint(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(paintColor);
+        drawPath = new Path();
     }
 
     @Override
-    protected void onDraw(Canvas canvas){
-
-        mDrawable.draw(canvas);
-
+    public void onDraw(Canvas canvas){
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent m){
+        float touchX = m.getX();
+        float touchY = m.getY();
+
+        switch (m.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
 }
--- app/src/main/java/utdallas/kam140430/androidfingerpaint/DrawExercise.java
+++ app/src/main/java/utdallas/kam140430/androidfingerpaint/DrawExercise.java
@@ -1,13 +1,44 @@
 package utdallas.kam140430.androidfingerpaint;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class DrawExercise extends AppCompatActivity {
+
+    public Court paintCourt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_exercise);
+        paintCourt = (Court)findViewById(R.id.court_id);
+
+        RadioGroup colorSwitches = (RadioGroup) findViewById(R.id.color_button_switches);
+        colorSwitches.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.redRadioButton:
+                        paintCourt.paintColor = Color.RED;
+                        break;
+                    case R.id.greenRadioButton:
+                        paintCourt.paintColor = Color.GREEN;
+
+                        break;
+                    case R.id.blueRadioButton:
+                        paintCourt.paintColor = Color.BLUE;
+                        break;
+                    case R.id.blackRadioButton:
+                        paintCourt.paintColor = Color.BLACK;
+                        break;
+                }
+            }
+        });
+
+        //intially setting red paint
+        colorSwitches.check(R.id.redRadioButton);
     }
+
 }
